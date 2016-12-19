79231d56d2515383180204faeba7e1179e7a3d28
Nicholas Day <nick@nickendo.com>
2016-12-19 14:12:16 -0500

    i3 config + urxvt url clicking

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # install infinality + add repos for arch
-cd ~
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
-mkdir .vim
+rm -rf ~/.vim
+mkdir ~/.vim
 sh /tmp/installer.sh ~/.vim
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 git clone https://github.com/chriskempson/base16-xresources.git ~/.config/base16-xresources
